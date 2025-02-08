a8cf36166689a5bcab6bc50fc720f9b181395856
zevanlop7 <EVER.LOPEZ2612@GMAIL.COM>
2025-02-07 22:37:17 -0500

    Creación de la clase Mascota y sus funciones

--- main.py
+++ main.py
@@ -18,3 +18,56 @@ servicios = ["consulta", "vacunación", "esterilizacion", "spa"]
     Mascota(2, "lucita", "Perro", "Crillo", "5")
 ]   """
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Mascota:             #Se crea una clase mascota 
+        id_counter = 1                                           #se iniciara el contador para mascotas en 1
+        def __init__(self, nombre, especie, raza, edad):
+            self.id = SistemaVeterinaria.Mascota.id_counter
+            self.nombre = nombre
+            self.especie = especie
+            self.raza = raza
+            self.edad = edad
+            self.historia_clinica = []   # cada mascota tiene una historia clinica
+            SistemaVeterinaria.Mascota.id_counter += 1
+
+        def agregar_cita(self, cita):
+            self.historia_clinica.append(cita)
+
+        def mostrar_historial(self):                         #funcion para mostrar la información de una cita
+            if not self.historia_clinica:
+                print(f"No hay citas registradas para {self.nombre}")
+            else:
+                print(f"\nHistorial de las citas para {self.nombre}")
+                for cita in self.historia_clinica:
+                    print(f" {cita.id}- {cita.fecha} a las {cita.hora}, servicio: {cita.servicio}, veterinario: {cita.veterinario}")
+
+        #def actualizar_cita(self,cita):
+        """def actualizar_cita(self, **kwargs):
+            for clave, valor in kwargs.items():
+                if hasattr(self, clave):
+                    setattr(self, clave, valor)"""
